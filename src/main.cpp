54e8ad3de639c0d525860adb8f846944009a49ed
bullydd <clubdeveloppeursenegal@gmail.com>
2024-12-10 11:25:08 +0000

    add src and glad

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/**
+ *  @file   : main.cpp
+ *  @author : bullydd
+ *  @brief  : OpenGL SDL startup program.
+ */
+
+#include <SDL2/SDL.h>
+#include <iostream>
+
+
+int main(int argc, char* argv[]) {
+    
+    return EXIT_SUCCESS;
+}
